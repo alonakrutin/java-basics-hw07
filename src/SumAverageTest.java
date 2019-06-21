383f72eb3edec6f0d8e47ed05738a2a7e8b0f4f8
Alona Krutin <alonka.krutin@gmail.com>
2019-06-21 08:55:33 +0300

    HW07

--- src/SumAverageTest.java
+++ src/SumAverageTest.java
@@ -1,69 +1,74 @@
-import static org.testng.Assert.assertEquals;
-
-import java.util.stream.IntStream;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.stream.IntStream;
+
+import static org.testng.Assert.assertEquals;
+
 public class SumAverageTest {
 
-  /**
-   * Test data for lower and ipper bounds.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "Sum")
-  public static Object[][] sum() {
-    return new Object[][]{
-        {1, 100, 5050},
-        {-100, 100, 0},
-        {1, 1, 1}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for lower and ipper bounds.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "Sum")
+    public static Object[][] sum() {
+        return new Object[][]{
+                {1, 100, 5050},
+                {-100, 100, 0},
+                {1, 1, 1},
+                {2, 4, 9},
+                {10, 12, 33}
+                // TODO add 2 more test data here
+        };
+    }
 
-  /**
-   * Test data for lower and ipper bounds.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "Average")
-  public static Object[][] average() {
-    return new Object[][]{
-        {1, 100, 50.5},
-        {-100, 100, 0.0},
-        {1, 1, 1.0}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for lower and ipper bounds.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "Average")
+    public static Object[][] average() {
+        return new Object[][]{
+                {1, 100, 50.5},
+                {-100, 100, 0.0},
+                {1, 1, 1.0},
+                {2, 3, 2.5},
+                {10, 12, 11}
 
-  @Test(dataProvider = "Sum")
-  public void testSumArray(int lowerBound, int upperBound, int expectedResult) {
-    int actualResult = SumAverage.sum(lowerBound, upperBound);
+                // TODO add 2 more test data here
+        };
+    }
 
-    assertEquals(actualResult, expectedResult, "Sum is not correct");
-  }
+    @Test(dataProvider = "Sum")
+    public void testSumArray(int lowerBound, int upperBound, int expectedResult) {
+        int actualResult = SumAverage.sum(lowerBound, upperBound);
 
-  @Test(dataProvider = "Sum")
-  public void testSum1(int lowerBound, int upperBound, int expectedResult) {
-    int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
-    int actualResult = SumAverage.sum(numbers);
+        assertEquals(actualResult, expectedResult, "Sum is not correct");
+    }
 
-    assertEquals(actualResult, expectedResult, "Sum is not correct");
-  }
+    @Test(dataProvider = "Sum")
+    public void testSum1(int lowerBound, int upperBound, int expectedResult) {
+        int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
+        int actualResult = SumAverage.sum(numbers);
 
-  @Test(dataProvider = "Average")
-  public void testAverage(int lowerBound, int upperBound, double expectedResult) {
-    double actualResult = SumAverage.average(lowerBound, upperBound);
+        assertEquals(actualResult, expectedResult, "Sum is not correct");
+    }
 
-    assertEquals(actualResult, expectedResult, "Average is not correct");
-  }
+    @Test(dataProvider = "Average")
+    public void testAverage(int lowerBound, int upperBound, double expectedResult) {
+        double actualResult = SumAverage.average(lowerBound, upperBound);
 
-  @Test(dataProvider = "Average")
-  public void testAverageArray(int lowerBound, int upperBound, double expectedResult) {
-    int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
-    double actualResult = SumAverage.average(numbers);
+        assertEquals(actualResult, expectedResult, "Average is not correct");
+    }
 
-    assertEquals(actualResult, expectedResult, "Average is not correct");
-  }
+    @Test(dataProvider = "Average")
+    public void testAverageArray(int lowerBound, int upperBound, double expectedResult) {
+        int[] numbers = IntStream.range(lowerBound, upperBound + 1).toArray();
+        double actualResult = SumAverage.average(numbers);
+
+        assertEquals(actualResult, expectedResult, "Average is not correct");
+    }
 }
